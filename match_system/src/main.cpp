525aade2e775acdfb77d711d47e73b8c561e0ae3
Li jixiang <2386730758@qq.com>
2023-10-18 22:28:32 +0800

    match server:5.0

--- match_system/src/main.cpp
+++ match_system/src/main.cpp
@@ -51,7 +51,7 @@ class Pool{
         {
             printf("Match Result: %d %d \n", a, b);
 
-            std::shared_ptr<TTransport> socket(new TSocket("123.57.47.211", 9090));
+            std::shared_ptr<TTransport> socket(new TSocket("localhost", 9090));
             std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
             std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
             SaveClient client(protocol);
@@ -59,35 +59,51 @@ class Pool{
             try {
                 transport->open();
 
-                int res = client.save_data("acs_441","e27bb88c", a , b);
-
-                if (!res)puts("success");
-                else puts("failed");
+            //    int res = client.save_data("acs_441","e27bb88c", a , b);
+                puts("success");
+              //  if (!res)puts("success");
+               // else puts("failed");
 
                 transport->close();
             } catch (TException& tx) {
                 cout << "ERROR: " << tx.what() << endl;
             }
         }
+        bool check_match(uint32_t i , uint32_t j)
+        {
+            auto a = users[i], b =users[j];
 
+            int dt = abs(a.score - b.score);
+            int a_max_dif = wt[i]*50;
+            int b_max_dif = wt[j]*50;
+
+            return dt<=a_max_dif && dt<=b_max_dif;
+        }
         void match()
         {
+            for(uint32_t i = 0;i<wt.size();i++)wt[i]++;//等待秒数+1   
             while(users.size()>1)
             {
-                sort(users.begin(),users.end(),[&](User& a,User& b){
-                        return a.score < b.score;
-                        });
                 bool flag=true;
-                for (uint32_t i= 1 ; i< users.size();i++)
+                for (uint32_t i= 0 ; i< users.size();i++)
                 {
-                    auto a = users[i-1], b = users[i];
-                    if ( b.score - a.score <=50)
+                    for( uint32_t j =i+1;j<users.size();j++)
                     {
-                        users.erase(users.begin()+i-1,users.begin()+i+1);
-                        save_result(a.id,b.id);
-                        flag=false;
-                        break;
+                        if(check_match(i,j))
+                        {
+                            auto a = users[i],b=users[j];
+                            users.erase(users.begin()+j);
+                            users.erase(users.begin()+i);
+                            wt.erase(wt.begin()+j);
+                            wt.erase(wt.begin()+i);
+                            save_result(a.id, b.id);
+                            flag=false;
+                            break;
+                        }
                     }
+
+                    if(!flag)break;
+
                 }
                 if(flag)break;
             }
@@ -95,6 +111,7 @@ class Pool{
         void add(User user)
         {
             users.push_back(user);
+            wt.push_back(0);
         }
         void remove(User user)
         {
@@ -102,11 +119,13 @@ class Pool{
                 if(users[i].id==user.id)
                 {
                     users.erase(users.begin()+i);
+                    wt.erase(wt.begin()+i);
                     break;
                 }
         }
     private:
         vector<User>users;
+        vector<int> wt; //等待时间:
 }pool;
 
 class MatchHandler : virtual public MatchIf {
@@ -170,8 +189,6 @@ void consume_task()
             //do task:
             if (task.type == "add") pool.add(task.user);
             else if (task.type == "remove")pool.remove(task.user);
-
-            pool.match();
         }
     }
 }
